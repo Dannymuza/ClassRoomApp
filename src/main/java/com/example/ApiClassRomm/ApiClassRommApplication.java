0169e34d33b845961c0d06abe39704dc17a9c817
dannymuza <danale202@gmail.com>
2025-03-07 08:25:34 -0500

    feat se crea proyecto

---
+++ src/main/java/com/example/ApiClassRomm/ApiClassRommApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiClassRomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClassRommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClassRommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiClassRomm/ApiClassRommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiClassRomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClassRommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
