ca59f6421df696098d46dafc6d4beb2d5f555450
dannymuza <danale202@gmail.com>
2025-04-04 10:21:16 -0500

    feat se crea servicio teacher

--- src/main/java/com/example/ApiClassRomm/Service/Teacherservice.java
+++ src/main/java/com/example/ApiClassRomm/Service/Teacherservice.java
@@ -5,6 +5,8 @@ import com.example.ApiClassRomm.repository.IteacherRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class Teacherservice {
     @Autowired //inyeccion de dependencias
@@ -12,15 +14,33 @@ public class Teacherservice {
     //en el servicio Implementamos  los metodos  que necesitemos para ofrecer
     //GUARDAR
 
-        public Teacher saveTeacher(Teacher dataTeacher)throws Exception{
-            try{
+        public Teacher saveTeacher(Teacher dataTeacher)throws Exception {
+            try {
                 return this.repositorie.save(dataTeacher);
-            }catch(Exception mistake){
-                throw new Exception(mistake.getMessage());
-}
-        //MODIFICAR
+            } catch (Exception error) {
+                throw new Exception(error.getMessage());
+            }
+        }
+            //MODIFICAR
+             public Teacher modifyTeacher (Integer user_id, Teacher dataTeacher ) throws Exception {
+
+                try {
+                    Optional<Teacher> searchTeacher = this.repositorie.findById(user_id);
+
+                    if ( searchTeacher.isPresent()){
+                        searchTeacher.get().setSpecialty (dataTeacher.getSpecialty());
+                        return this.repositorie.save(searchTeacher.get());
+
+                    }
+                        else{
+                            throw new Exception("no encontro el usuario por id");
+                        }
+                } catch (Exception error){
+                    throw new Exception(error.getMessage());}
+
+            }
         //BUSCAR POR ID
         //BUSCAR TODOS
         //ELIMINAR
     }
-}
+
